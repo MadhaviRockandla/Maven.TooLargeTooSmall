2143a0e183e0d38029cd722b0d72fa6cb931db13
MadhaviRockandla <rokandla@gmail.com>
2022-03-01 23:01:58 -0500

    completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int guessNum = rand.nextInt(500);
+        Scanner input = new Scanner(System.in);
+        int guess;
+
+
+        System.out.println("Enter number between 1 and 500: ");
+        guess = input.nextInt();
+
+
+        if (guess == guessNum) ;
+
+        else if (guess < guessNum) {
+            System.out.println("Too small!");
+        } else
+        {
+            System.out.println("Too large!");
+        }
     }
+
 }
+
